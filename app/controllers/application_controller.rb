371ff867ba345e984c9282cc33e5baa57b55d8cb
Collin Schneider <collin@jastr.co>
2016-10-06 18:19:35 -0400

    trying to clean up mail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 require 'Util'
 require 'BlueBirdEmail'
-require 'sendgrid-ruby'
-include SendGrid
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
