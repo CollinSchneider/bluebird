892a20c4874cb1dcaaed6fcb31791c81ea735b30
Collin Schneider <collin@jastr.co>
2016-10-25 23:59:20 -0400

    improves company views and logos

--- app/controllers/api/company_controller.rb
+++ app/controllers/api/company_controller.rb
@@ -1,4 +1,10 @@
 class Api::CompanyController < ApiController
 
+  def remove_logo
+    company = Company.find(params[:company])
+    company.logo.clear
+    company.save!
+    return redirect_to request.referrer
+  end
 
 end
--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,14 +3,12 @@ class CompaniesController < ApplicationController
   def update
     company = Company.find(params[:id])
     if company.user_id == current_user.id
+      og_file_url = company.logo.url
+      og_logo = company.logo
       company.update(company_params)
-      if company.save(validate: false)
-        flash[:success] = "Company updated!"
-        return redirect_to request.referrer
-      else
-        flash[:error] = company.errors
-        return redirect_to request.referrer
-      end
+      company.save(validate: false)
+      flash[:success] = "Company updated!"
+      return redirect_to request.referrer
     end
   end
 
