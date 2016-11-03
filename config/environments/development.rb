b38a4d0acc31eabe6bae5f3adf86a3087a921ee6
Collin Schneider <collin@jastr.co>
2016-11-03 14:13:47 -0400

    adds errors to production.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,15 +18,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'localhost:3000'}
 
   config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address: 'smtp.gmail.com',
-  #   port: '587',
-  #   domain: 'gmail.com',
-  #   authentication: 'plain',
-  #   user_name: ENV['GMAIL_USERNAME'],
-  #   password: ENV['GMAIL_PASSWORD'],
-  #   enable_starttls_auto: true
-  # }
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
